c61e97e51d791bb8e4b6c814cf13a04621d4d7ff
Tanmay Paul <tanmaypaulofficial@gmail.com>
2024-04-16 19:38:48 +0530

    customer register re-designed

--- src/components/Navbar/MasterFile/CustomerRegister.js
+++ src/components/Navbar/MasterFile/CustomerRegister.js
@@ -6,11 +6,12 @@ import {
   Typography,
   MenuItem,
   Grid,
+  InputLabel,
 } from "@mui/material";
 
 const CustomerRegister = () => {
   const [formData, setFormData] = useState({
-    name: "",
+    customerName: "",
     contactPersons: "",
     address: "",
     phoneNo: "",
@@ -31,14 +32,14 @@ const CustomerRegister = () => {
 
   const handleClear = () => {
     setFormData({
-      name: "",
+      customerName: "",
       contactPersons: "",
       address: "",
       phoneNo: "",
       mobileNo: "",
       openingBalance: "",
       discount: "",
-      validUpto: null,
+      validUpto: "mm/dd/yyyy",
       customerType: "",
       discountCategory: "",
       additionalCharges: "",
@@ -46,180 +47,234 @@ const CustomerRegister = () => {
   };
 
   const handleFormChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
+    const {name, value} = e.target;
+    console.log(":---> ", value);
+    setFormData({
+      ...formData,
       [name]: value,
-    }));
+    });
   };
 
   return (
     <form>
       <Box sx={{ p: 2, width: "900px" }}>
-        <Typography variant="h5" component="div" gutterBottom>
-          Customer Information
-        </Typography>
         <Typography variant="subtitle1" gutterBottom>
-          Customer Details
+          CUSTOMER REGISTER:
         </Typography>
 
         <Grid container spacing={2}>
-          <Grid item xs={2}>
-            <TextField
-              name="name"
-              label="Name of Customer/Company"
-              variant="outlined"
-              fullWidth
-              className="input-field"
-              value={formData.name}
-              onChange={handleFormChange}
-            />
+          <Grid item xs={4}>
+            <div className="input-wrapper">
+              <InputLabel htmlFor="customerName" className="input-label label-adjustment">
+                Name of Customer :
+              </InputLabel>
+              <TextField
+                fullWidth
+                size="small"
+                name="customerName"
+                variant="outlined"
+                className="input-field field-adjustment"
+                value={formData.customerName}
+                onChange={(e) => handleFormChange(e)}
+              />
+            </div>
           </Grid>
-          <Grid item xs={2}>
-            <TextField
-              name="contactPersons"
-              label="Contact Persons"
-              variant="outlined"
-              fullWidth
-              className="input-field"
-              value={formData.contactPersons}
-              onChange={handleFormChange}
-            />
+          <Grid item xs={4}>
+            <div className="input-wrapper">
+              <InputLabel htmlFor="contactPersons" className="input-label label-adjustment">
+                Contact Persons :
+              </InputLabel>
+              <TextField
+                fullWidth
+                size="small"
+                name="contactPersons"
+                variant="outlined"
+                className="input-field field-adjustment"
+                value={formData.contactPersons}
+                onChange={(e) => handleFormChange(e)}
+              />
+            </div>
           </Grid>
-          <Grid item xs={2}>
-            <TextField
-              name="address"
-              label="Address"
-              variant="outlined"
-              fullWidth
-              className="input-field"
-              value={formData.address}
-              onChange={handleFormChange}
-            />
+          <Grid item xs={4}>
+            <div className="input-wrapper">
+              <InputLabel htmlFor="address" className="input-label label-adjustment">
+                Address :
+              </InputLabel>
+              <TextField
+                fullWidth
+                size="small"
+                name="address"
+                variant="outlined"
+                className="input-field field-adjustment"
+                value={formData.address}
+                onChange={(e) => handleFormChange(e)}
+              />
+            </div>
           </Grid>
-          <Grid item xs={2}>
-            <TextField
-              name="phoneNo"
-              label="Phone No."
-              variant="outlined"
-              fullWidth
-              className="input-field"
-              type="number"
-              value={formData.phoneNo}
-              onChange={handleFormChange}
-            />
+          <Grid item xs={4}>
+            <div className="input-wrapper">
+              <InputLabel htmlFor="phoneNo" className="input-label label-adjustment">
+                Phone No. :
+              </InputLabel>
+              <TextField
+                fullWidth
+                size="small"
+                name="phoneNo"
+                variant="outlined"
+                className="input-field field-adjustment"
+                type="number"
+                value={formData.phoneNo}
+                onChange={(e) => handleFormChange(e)}
+              />
+            </div>
           </Grid>
-          <Grid item xs={2}>
-            <TextField
-              name="mobileNo"
-              label="Mobile No."
-              variant="outlined"
-              fullWidth
-              className="input-field"
-              type="number"
-              value={formData.mobileNo}
-              onChange={handleFormChange}
-            />
+          <Grid item xs={4}>
+            <div className="input-wrapper">
+              <InputLabel htmlFor="mobileNo" className="input-label label-adjustment">
+                Mobile No. :
+              </InputLabel>
+              <TextField
+                fullWidth
+                size="small"
+                name="mobileNo"
+                variant="outlined"
+                className="input-field field-adjustment"
+                type="number"
+                value={formData.mobileNo}
+                onChange={(e) => handleFormChange(e)}
+              />
+            </div>
           </Grid>
-          <Grid item xs={2}>
-            <TextField
-              name="openingBalance"
-              label="Opening Balance (Rs.)"
-              variant="outlined"
-              fullWidth
-              className="input-field"
-              type="number"
-              inputProps={{ min: 0 }}
-              value={formData.openingBalance}
-              onChange={handleFormChange}
-            />
+          <Grid item xs={4}>
+            <div className="input-wrapper">
+              <InputLabel htmlFor="openingBalance" className="input-label label-adjustment">
+                Opening Balance (Rs.) :
+              </InputLabel>
+              <TextField
+                fullWidth
+                size="small"
+                name="openingBalance"
+                variant="outlined"
+                className="input-field field-adjustment"
+                type="number"
+                inputProps={{ min: 0 }}
+                value={formData.openingBalance}
+                onChange={(e) => handleFormChange(e)}
+              />
+            </div>
           </Grid>
-          <Grid item xs={2}>
-            <TextField
-              name="discount"
-              label="Discount (%)"
-              variant="outlined"
-              fullWidth
-              className="input-field"
-              type="number"
-              value={formData.discount}
-              onChange={handleFormChange}
-            />
+          <Grid item xs={4}>
+            <div className="input-wrapper">
+              <InputLabel htmlFor="discount" className="input-label label-adjustment">
+                Discount (%) :
+              </InputLabel>
+              <TextField
+                fullWidth
+                size="small"
+                name="discount"
+                variant="outlined"
+                className="input-field field-adjustment"
+                type="number"
+                value={formData.discount}
+                onChange={(e) => handleFormChange(e)}
+              />
+            </div>
           </Grid>
-          <Grid item xs={2}>
-            <TextField
-              fullWidth
-              type="date"
-              label="Valid Upto"
-              name="validUpto"
-              className="input-field"
-              variant="outlined"
-              value={formData.validUpto}
-              onChange={handleFormChange}
-            />
+          <Grid item xs={4}>
+            <div className="input-wrapper">
+              <InputLabel htmlFor="validUpto" className="input-label label-adjustment">
+                Valid Upto :
+              </InputLabel>
+              <TextField
+                fullWidth
+                size="small"
+                name="validUpto"
+                variant="outlined"
+                className="input-field field-adjustment"
+                type="date"
+                value={formData.validUpto}
+                onChange={(e) => handleFormChange(e)}
+              />
+            </div>
           </Grid>
-          <Grid item xs={2}>
-            <TextField
-              name="customerType"
-              select
-              label="Customer Type"
-              variant="outlined"
-              fullWidth
-              className="input-field"
-              value={formData.customerType}
-              onChange={handleFormChange}
-            >
-              <MenuItem value="cash">Cash</MenuItem>
-              <MenuItem value="online">Online</MenuItem>
-            </TextField>
+          <Grid item xs={4}>
+            <div className="input-wrapper">
+              <InputLabel htmlFor="customerType" className="input-label label-adjustment">
+                Customer Type :
+              </InputLabel>
+              <TextField
+                select
+                fullWidth
+                size="small"
+                name="customerType"
+                variant="outlined"
+                className="input-field field-adjustment"
+                value={formData.customerType}
+                onChange={(e) => handleFormChange(e)}
+              >
+                <MenuItem value="cash">Cash</MenuItem>
+                <MenuItem value="online">Online</MenuItem>
+              </TextField>
+            </div>
           </Grid>
-          <Grid item xs={2}>
-            <TextField
-              name="discountCategory"
-              select
-              label="Discount Category"
-              variant="outlined"
-              fullWidth
-              className="input-field"
-              value={formData.discountCategory}
-              onChange={handleFormChange}
-            >
-              <MenuItem value="1">1</MenuItem>
-              <MenuItem value="2">2</MenuItem>
-            </TextField>
+          <Grid item xs={4}>
+            <div className="input-wrapper">
+              <InputLabel htmlFor="discountCategory" className="input-label label-adjustment">
+                Discount Category :
+              </InputLabel>
+              <TextField
+                select
+                fullWidth
+                size="small"
+                name="discountCategory"
+                variant="outlined"
+                className="input-field field-adjustment"
+                value={formData.discountCategory}
+                onChange={(e) => handleFormChange(e)}
+              >
+                <MenuItem value="1">1</MenuItem>
+                <MenuItem value="2">2</MenuItem>
+              </TextField>
+            </div>
           </Grid>
-          <Grid item xs={2}>
-            <TextField
-              name="additionalCharges"
-              label="Additional Charges (%)"
-              variant="outlined"
-              fullWidth
-              className="input-field"
-              type="number"
-              value={formData.additionalCharges}
-              onChange={handleFormChange}
-            />
+          <Grid item xs={4}>
+            <div className="input-wrapper">
+              <InputLabel htmlFor="additionalCharges" className="input-label label-adjustment">
+                Additional Charges (%) :
+              </InputLabel>
+              <TextField
+                fullWidth
+                size="small"
+                name="additionalCharges"
+                variant="outlined"
+                className="input-field field-adjustment"
+                type="number"
+                value={formData.additionalCharges}
+                onChange={(e) => handleFormChange(e)}
+              />
+            </div>
           </Grid>
         </Grid>
+
         <Box
           sx={{
             display: "flex",
             justifyContent: "flex-end",
             gap: 2,
-            marginTop: 2
+            marginTop: 2,
           }}
         >
           <Button
-            color="primary"
-            size="large"
-            variant="outlined"
+            color="success"
+            size="medium"
+            variant="contained"
             onClick={handleSave}
           >
             Save
           </Button>
           <Button
-            color="error"
-            size="large"
+            color="warning"
+            size="medium"
             variant="outlined"
             onClick={handleClear}
           >
