06379a7ff3550dd441ed7d9e32e66350b32a121e
unknown <kishortalukdar55@gmail.com>
2024-04-17 10:46:53 +0530

    folder stracture

---
+++ src/constants/index.js
@@ -0,0 +1,16 @@
+export const server_url = 'http://localhost:8080/api/v1';
+
+export var dataConstant = new Object();
+
+dataConstant.status = [
+    {
+        'id':1,
+        'name': 'Active',
+        'value' : "Active"
+    },
+    {
+        'id':2,
+        'name': 'InActive',
+        'value' : "InActive" 
+    }
+];
