b20e7828398aacf435b9e22094a012f187a5223a
Tanmay Paul <tanmaypaulofficial@gmail.com>
2024-07-22 13:57:57 +0530

    env created and url changed

--- src/utils/apiDomain.js
+++ src/utils/apiDomain.js
@@ -1,1 +1,5 @@
-export const url = 'https://api.wine.tdrsoftware.in/api/v1';
+// export const url = 'https://api.wine.tdrsoftware.in/api/v1';
+
+export const url = process.env.REACT_APP_API_URL + '/api/v1';
+
+// console.log("url: ",url)
