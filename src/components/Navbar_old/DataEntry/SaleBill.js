e611b219eb250e877297cb287a5f94bd38d2c65a
Tanmay Paul <tanmaypaulofficial@gmail.com>
2024-10-28 18:10:30 +0530

    save & print implemented

--- src/components/Navbar_old/DataEntry/SaleBill.js
+++ src/components/Navbar_old/DataEntry/SaleBill.js
@@ -4,6 +4,11 @@ import React, { useCallback, useEffect, useRef, useState } from "react";
 import {
   Box,
   Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Divider,
   FormControlLabel,
   Grid,
   InputLabel,
@@ -14,6 +19,7 @@ import {
   ThemeProvider,
   Typography,
 } from "@mui/material";
+import { toWords } from "number-to-words";
 import { getAllCustomer } from "../../../services/customerService";
 import { NotificationManager } from "react-notifications";
 import debounce from "lodash.debounce";
@@ -123,6 +129,7 @@ const SaleBill = () => {
   const [printData, setPrintData] = useState([]);
   const [printTotalValues, setPrintTotalValues] = useState([]);
   const [isAutoBillPrint, setIsAutoBillPrint] = useState(false);
+  const [isSaveAndPrintClicked, setIsSaveAndPrintClicked] = useState(false);
   // console.log("isAutoBillPrint", isAutoBillPrint)
 
   const tableRef = useRef(null);
@@ -411,6 +418,20 @@ const SaleBill = () => {
       receiptMode1: "",
       receiptMode2: "",
     }));
+  };
+
+  const printModalStyles = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "90mm",
+    bgcolor: "background.paper",
+    boxShadow: 24,
+    p: 2,
+    borderRadius: 2,
+    textAlign: "center",
+    display: 'none'
   };
 
   const itemNameSearch = debounce(async (itemName, storeName) => {
@@ -1135,6 +1156,7 @@ const SaleBill = () => {
       parseFloat(totalValues.adjustment || 0) +
       parseFloat(totalValues.taxAmt || 0);
 
+      // console.log("netAmt: " + netAmt)
     setTotalValues({
       ...totalValues,
       totalVolume: calculatedTotalVolume.toFixed(0),
@@ -1448,6 +1470,11 @@ const SaleBill = () => {
         if (licenseDetails?.autoBillPrint === "YES") {
           handlePrint();
         }
+
+        if(isSaveAndPrintClicked){
+          // alert("triggered save and print...")
+          handlePrint();
+        };
 
         // if (licenseDetails?.autoBillPrint === "YES") {
         //   setIsAutoBillPrint(true);
@@ -1952,14 +1979,14 @@ const SaleBill = () => {
   const calculateNetAmount = () => {
     const totalVolume = salesData.reduce(
       (total, item) =>
-        total + (parseFloat(item.volume) || 0) * (parseInt(item.pcs) || 0),
+        total + (parseFloat(item.volume) || 0) * ((parseInt(item.pcs) - (parseInt(item.brk))) || 0),
       0
     );
 
     const flBeerVolume = salesData.reduce((total, item) => {
       if (item.group === "FL" || item.group === "BEER") {
         return (
-          total + (parseFloat(item.volume) || 0) * (parseInt(item.pcs) || 0)
+          total + (parseFloat(item.volume) || 0) * ((parseInt(item.pcs) - (parseInt(item.brk))) || 0)
         );
       }
       return total;
@@ -1968,7 +1995,7 @@ const SaleBill = () => {
     const imlVolume = salesData.reduce((total, item) => {
       if (item.group === "IML") {
         return (
-          total + (parseFloat(item.volume) || 0) * (parseInt(item.pcs) || 0)
+          total + (parseFloat(item.volume) || 0) * ((parseInt(item.pcs) - (parseInt(item.brk))) || 0)
         );
       }
       return total;
@@ -1999,6 +2026,7 @@ const SaleBill = () => {
     const netAmt = grossAmt - splDiscAmount - adjustment;
 
     const newNetAmount = netAmt - totalDiscount;
+    // console.log("newNetAmount: ",newNetAmount)
 
     setTotalValues({
       ...totalValues,
@@ -2007,14 +2035,17 @@ const SaleBill = () => {
       imlVolume: imlVolume.toFixed(2),
       totalPcs: totalPcs,
       grossAmt: grossAmt.toFixed(2),
-      receiptMode1: billNumber
-        ? totalValues.receiptMode1
-        : newNetAmount.toFixed(2),
+      receiptMode1: billNumber ?
+         totalValues.receiptMode1 :  newNetAmount.toFixed(2),
+      // receiptAmt: totalValues.receiptMode2 ? newNetAmount.toFixed(2) : totalValues.receiptAmt,
       splDiscAmount: (splDiscAmount + totalDiscount).toFixed(0),
       discountAmt: totalDiscount.toFixed(0),
       netAmt: newNetAmount.toFixed(2),
     });
   };
+
+  // console.log("billNumber:",billNumber);
+  // console.log("totalValues.receiptMode1: ",totalValues.receiptMode1);
 
   useEffect(() => {
     calculateNetAmount();
@@ -2059,6 +2090,33 @@ const SaleBill = () => {
   //     netAmt: netAmt.toFixed(2),
   //   };
   // };
+
+  // const dataToPrint = isSplitPrinted ? printData : salesData;
+  //   const valuesToPrint = isSplitPrinted ? printTotalValues : totalValues;
+  
+  //   const { totalVolume, totalPcs, grossAmt, splDiscAmount, netAmt } = valuesToPrint;
+
+  //   const amountInWords = (amount) => {
+  //     if (isNaN(amount) || amount === undefined || amount === null) {
+  //       return "Zero Only";
+  //     }
+  //     return toWords(amount) + " Only";
+  //   };
+
+  //   const saleBillData = {
+  //     customer: formData?.customerName?.name,
+  //     billNo: billNumber,
+  //     discount: parseFloat(splDiscAmount),
+  //     adjustment: parseFloat(valuesToPrint?.adjustment),
+  //     date: new Date(formData?.billDate).toLocaleDateString(),
+  //     time: new Date(formData?.billDate).toLocaleTimeString(),
+  //     items: dataToPrint || [],
+  //     totalQty: totalPcs,
+  //     grossAmt: parseFloat(grossAmt),
+  //     totalBL: parseFloat(totalVolume) * parseFloat(totalPcs),
+  //     netAmount: parseFloat(netAmt),
+  //     amountInWords: amountInWords(parseFloat(netAmt)),
+  //   };
 
   useEffect(() => {
     if (billDateRef.current) {
@@ -3034,6 +3092,24 @@ const SaleBill = () => {
           PRINT
         </Button>
         <Button
+          color="secondary"
+          size="small"
+          variant="contained"
+          onClick={() => {
+            if (salesData.length > 0 && !billNumber && !billNoEditable) 
+              setIsSaveAndPrintClicked(true);
+              handleCreateSale();
+          }}
+          sx={{
+            marginRight: 1,
+            padding: "4px 10px",
+            fontSize: "11px",
+          }}
+          // disabled={!billNumber && !billNoEditable}
+        >
+          SAVE & PRINT
+        </Button>
+        <Button
           ref={saveButtonRef}
           color="success"
           size="small"
@@ -3063,6 +3139,191 @@ const SaleBill = () => {
         isSplitPrinted={isSplitPrinted}
         printTotalValues={printTotalValues}
       />
+
+
+
+        
+      {/* <Dialog 
+      // open={isSaveAndPrintClicked || isSplitPrinted} onClose={() => setIsSaveAndPrintClicked(false)} 
+      sx={printModalStyles}>
+        <Box ref={printModalRef} sx={{ p: 1, width: "80mm" }}>
+          <Typography
+            variant="h2"
+            component="h1"
+            align="center"
+            sx={{ fontSize: "12px", fontWeight: "bold", mb: 1 }}
+          >
+            CASH MEMO
+          </Typography>
+          <Typography
+            variant="h2"
+            component="h1"
+            align="center"
+            sx={{ fontSize: "12px" }}
+          >
+            {licenseDetails?.nameOfLicence}
+          </Typography>
+          <Typography
+            variant="h4"
+            component="h2"
+            align="center"
+            sx={{ fontSize: "12px" }}
+          >
+            {licenseDetails?.businessType}
+          </Typography>
+          <Typography
+            variant="h6"
+            component="h2"
+            align="center"
+            sx={{ fontSize: "10px" }}
+          >
+            {licenseDetails?.address}
+          </Typography>
+          <Typography
+            variant="h6"
+            component="h2"
+            align="center"
+            sx={{ fontSize: "10px" }}
+          >
+            {licenseDetails?.district || ""}
+          </Typography>
+
+          <Divider sx={{ my: 1 }} />
+
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              fontSize: "10px",
+            }}
+          >
+            <Typography variant="body1" sx={{ fontSize: "11px" }}>
+              {saleBillData.customer ? `Customer: ${saleBillData.customer}` : ""}
+            </Typography>
+            <Typography variant="body1" sx={{ fontSize: "11px" }}>
+              *{saleBillData.billNo || ""}*
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              fontSize: "10px",
+            }}
+          >
+            <Typography variant="body1" sx={{ fontSize: "11px" }}>
+              {saleBillData.billNo ? `Bill No.: ${saleBillData.billNo}` : ""}
+            </Typography>
+            <Typography variant="body1" sx={{ fontSize: "11px" }}></Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              fontSize: "10px",
+            }}
+          >
+            <Typography variant="body1" sx={{ fontSize: "10px" }}>
+              {saleBillData.date ? `Date: ${saleBillData.date}` : ""}
+            </Typography>
+            <Typography variant="body1" sx={{ fontSize: "10px" }}>
+              {saleBillData.time ? `Time: ${saleBillData.time}` : ""}
+            </Typography>
+          </Box>
+
+          <Divider sx={{ my: 1 }} />
+
+          <Typography variant="body2" sx={{ fontSize: "10px" }}>
+            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+              <Box sx={{ width: "25%", fontWeight: "bold" }}>BATCH</Box>
+              <Box sx={{ width: "35%", fontWeight: "bold" }}>ITEM</Box>
+              <Box sx={{ width: "10%", fontWeight: "bold" }}>QTY</Box>
+              <Box sx={{ width: "15%", fontWeight: "bold" }}>RATE</Box>
+              <Box sx={{ width: "15%", fontWeight: "bold" }}>AMT.</Box>
+            </Box>
+          </Typography>
+          {saleBillData.items?.map((item, index) => (
+            <Typography key={index} variant="body2" sx={{ fontSize: "10px" }}>
+              <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                <Box sx={{ width: "25%" }}>{item.batch}</Box>
+                <Box sx={{ width: "35%" }}>{item.itemName}</Box>
+                <Box sx={{ width: "10%" }}>{item.pcs}</Box>
+                <Box sx={{ width: "15%" }}>{item.rate}</Box>
+                <Box sx={{ width: "15%" }}>{item.amount.toFixed(2)}</Box>
+              </Box>
+            </Typography>
+          ))}
+
+          <Divider sx={{ my: 1 }} />
+
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              fontSize: "10px",
+            }}
+          >
+            <Typography variant="body1" sx={{ fontSize: "11px" }}>
+              Total Qty: {saleBillData.totalQty}
+            </Typography>
+            <Typography variant="body1" sx={{ fontSize: "11px" }}>
+              Gross Amt: {saleBillData.grossAmt?.toFixed(2)}
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              fontSize: "10px",
+            }}
+          >
+            <Typography variant="body1" sx={{ fontSize: "11px" }}>
+              {saleBillData.adjustment
+                ? `Adjustment: ${saleBillData.adjustment}`
+                : ""}
+            </Typography>
+            <Typography variant="body1" sx={{ fontSize: "11px" }}>
+              {saleBillData.discount
+                ? `Total Discount: ${saleBillData.discount}`
+                : ""}
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              fontSize: "10px",
+            }}
+          >
+            <Typography variant="body1" sx={{ fontSize: "11px" }}>
+              Total BL: {saleBillData.totalBL}
+            </Typography>
+            <Typography variant="body1" sx={{ fontSize: "11px" }}>
+              Net Amount: {saleBillData.netAmount?.toFixed(2)}
+            </Typography>
+          </Box>
+          <Typography
+            variant="body1"
+            align="center"
+            sx={{ fontSize: "10px" }}
+          >
+            Rs.: {saleBillData.amountInWords}
+          </Typography>
+
+          <Divider sx={{ my: 1 }} />
+
+          <Typography
+            variant="body1"
+            align="center"
+            sx={{ fontSize: "10px" }}
+          >
+            {licenseDetails?.billMessages}
+          </Typography>
+        </Box>
+      </Dialog> */}
+
+
+
     </ThemeProvider>
   );
 };
